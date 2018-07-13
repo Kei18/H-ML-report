bf030602e7a792d3c829bf34c61c03becf310488
Kei18 <okumura.k@coord.c.titech.ac.jp>
2018-07-13 10:28:35 +0900

    add lasso with accelerated proximal gradient

--- src/problem2.py
+++ src/problem2.py
@@ -28,7 +28,10 @@ def soft_threshold(q, mu):
 def phi_grad(w, A, mu):
     return 2 * np.dot(A, (w - mu))
 
-def lasso(A, mu, _lambda, repeat_num=50, w_init=np.array([3, -1])):
+def q_scalar_func(t):
+    return (t - 1) / (t + 2)
+
+def lasso(A, mu, _lambda, repeat_num=50, accelerated=False, w_init=np.array([3, -1])):
     # obtain eigen value of 2A, then, use as the inverse of the lerning rate
     (eigs, vec) = np.linalg.eig(2*A)
     _gamma = np.max(eigs)
@@ -39,13 +42,19 @@ def lasso(A, mu, _lambda, repeat_num=50, w_init=np.array([3, -1])):
     w_hist = [w]
     # update weight
     for i in range(0, repeat_num):
-        w = soft_threshold_array(q, w - phi_grad(w, A, mu) / _gamma)
+        if accelerated and i > 1:
+            # accelerated proximal gradient update
+            v = w + q_scalar_func(i) * (w - w_hist[-2])
+            w = soft_threshold_array(q, v - phi_grad(v, A, mu) / _gamma)
+        else:
+            # proximal gradient update
+            w = soft_threshold_array(q, w - phi_grad(w, A, mu) / _gamma)
         w_hist.append(w)
         print("itr:%d" % i, w)
     return w_hist
 
 # for visualization
-def make_contour(w, A, mu, _lambda, step=0.05, xmin=-2, xmax=3.1, ymin=-2, ymax=3.1):
+def make_contour(A, mu, _lambda, step=0.05, xmin=-2, xmax=3.1, ymin=-2, ymax=3.1):
     x = np.arange(xmin, xmax, step)
     y = np.arange(ymin, ymax, step)
     loss = np.zeros((len(x), len(y)))
@@ -67,20 +76,28 @@ if __name__ == '__main__':
     w_opt_4 = np.array([0.64, 0.18])
     w_opt_6 = np.array([0.33, 0])
 
-    # implement lasso
+    # implement lasso with proximal gradient
     w_2 = lasso(A, mu, 2, repeat_num)
     w_4 = lasso(A, mu, 4, repeat_num)
     w_6 = lasso(A, mu, 6, repeat_num)
+
+    # implement lasso with acccelerated proximal gradient
+    w_a_2 = lasso(A, mu, 2, repeat_num, True)
+    w_a_4 = lasso(A, mu, 4, repeat_num, True)
+    w_a_6 = lasso(A, mu, 6, repeat_num, True)
 
     # compute dist from opt
     dist_2 = [np.sum(np.abs(w - w_opt_2)) for w in w_2]
     dist_4 = [np.sum(np.abs(w - w_opt_4)) for w in w_4]
     dist_6 = [np.sum(np.abs(w - w_opt_6)) for w in w_6]
+    dist_a_2 = [np.sum(np.abs(w - w_opt_2)) for w in w_a_2]
+    dist_a_4 = [np.sum(np.abs(w - w_opt_4)) for w in w_a_4]
+    dist_a_6 = [np.sum(np.abs(w - w_opt_6)) for w in w_a_6]
 
     # compute contour to plot data
-    (x, y, loss_2) = make_contour(w, A, mu, 2)
-    (x, y, loss_4) = make_contour(w, A, mu, 4)
-    (x, y, loss_6) = make_contour(w, A, mu, 6)
+    (x, y, loss_2) = make_contour(A, mu, 2)
+    (x, y, loss_4) = make_contour(A, mu, 4)
+    (x, y, loss_6) = make_contour(A, mu, 6)
 
     # plot result
     def plt_config_result():
@@ -92,6 +109,7 @@ if __name__ == '__main__':
         plt.xlabel("w_1")
         plt.ylabel("w_2")
 
+    # result of proximal gradient
     plt_config_result()
     plt.contour(x, y, loss_2, 50)
     plt.colorbar()
@@ -119,11 +137,44 @@ if __name__ == '__main__':
     plt.savefig(filename, pad_inches=0.05, transparent=True, bbox_inches='tight')
     plt.clf()
 
+    # result of accelerated proximal gradient
+    plt_config_result()
+    plt.contour(x, y, loss_2, 50)
+    plt.colorbar()
+    plt.plot([_w[0] for _w in w_2], [_w[1] for _w in w_a_2],
+             color="red", marker="o", ms=4, linewidth=1)
+    filename = os.path.join("figs", "p2_lasso_accelerated_result_lambda-2.pdf")
+    plt.savefig(filename, pad_inches=0.05, transparent=True, bbox_inches='tight')
+    plt.clf()
+
+    plt_config_result()
+    plt.contour(x, y, loss_4, 50)
+    plt.colorbar()
+    plt.plot([_w[0] for _w in w_4], [_w[1] for _w in w_a_4],
+             color="red", marker="o", ms=4, linewidth=1)
+    filename = os.path.join("figs", "p2_lasso_accelerated_result_lambda-4.pdf")
+    plt.savefig(filename, pad_inches=0.05, transparent=True, bbox_inches='tight')
+    plt.clf()
+
+    plt_config_result()
+    plt.contour(x, y, loss_6, 50)
+    plt.colorbar()
+    plt.plot([_w[0] for _w in w_6], [_w[1] for _w in w_a_6],
+             color="red", marker="o", ms=4, linewidth=1)
+    filename = os.path.join("figs", "p2_lasso_accelerated_result_lambda-6.pdf")
+    plt.savefig(filename, pad_inches=0.05, transparent=True, bbox_inches='tight')
+    plt.clf()
+
+
     # plot dist
     plt.figure(figsize=(6, 4))
     plt.plot(np.arange(repeat_num + 1), dist_2, label="lambda=2")
     plt.plot(np.arange(repeat_num + 1), dist_4, label="lambda=4")
     plt.plot(np.arange(repeat_num + 1), dist_6, label="lambda=6")
+    plt.plot(np.arange(repeat_num + 1), dist_a_2, label="a, lambda=2")
+    plt.plot(np.arange(repeat_num + 1), dist_a_4, label="a, lambda=4")
+    plt.plot(np.arange(repeat_num + 1), dist_a_6, label="a, lambda=6")
+
     plt.xlim(0, repeat_num)
     plt.yscale('log')
     plt.xlabel("t")
